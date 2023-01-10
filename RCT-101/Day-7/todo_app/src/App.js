a9279eb989ecad0e0267ee1ffb399a77c773647c
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-10 12:05:06 +0530

    file added

---
+++ RCT-101/Day-7/todo_app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <input placeholder="Add Something"/>
+       <button>Add Task</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
