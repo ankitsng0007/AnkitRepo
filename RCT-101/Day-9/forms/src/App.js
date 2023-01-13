fb9f96222505a3048d984b56a5b11d88c4bfa2eb
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-13 23:27:07 +0530

    file Added

--- RCT-101/Day-9/forms/src/App.js
+++ RCT-101/Day-9/forms/src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
+const initstate={
+  username:"",
+  email:"",
+  password:"",
+  country:""
+};
 function App() {
+  const [formData,setFormData]=useState(initstate);
+
+  const handleChange=(e)=>{
+    console.log(e.target.name)
+  };
+  console.log(formData);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <h1>Forms</h1>
+        <label>Username : 
+          <input placeholder='Enter Name'
+          name='username'
+          type="text"
+          value={formData.username}
+          onChange={handleChange}/>
+        </label>
+        <br/>
+        <br/>
+        <label>Email : 
+          <input placeholder='Enter Email'
+          name='email'
+          type="email"
+          value={formData.email}
+          onChange={handleChange}/>
+        </label>
+        <br/>
+        <br/>
+        <label>password : 
+          <input placeholder='Enter Password'
+          name='password'
+          type="password"
+          value={formData.password}
+          onChange={handleChange}/>
+        </label>
+        <br/>
+        <br/>
+        <label>Country : 
+          <select name="country" value={formData.country} onChange={handleChange}>
+            <option value="india">INDIA</option>
+            <option value="china">CHINA</option>
+            <option value="usa">USA</option>
+            <option value="canada">CANADA</option>
+          </select>
+        </label>
+      </form>
     </div>
   );
 }
