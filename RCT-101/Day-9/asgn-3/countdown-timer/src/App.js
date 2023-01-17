c785f300682703c50a4b78ff2f3e0aac484934be
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-17 23:01:25 +0530

    file Added

--- RCT-101/Day-9/asgn-3/countdown-timer/src/App.js
+++ RCT-101/Day-9/asgn-3/countdown-timer/src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useRef} from "react";
 
 function App() {
+  const [timer,setTimer] = useState(0)
+  const ref=useRef(null);
+
+  const startTimer=()=>{
+    ref.current = setInterval(()=>{
+      setTimer((prevTimer)=>prevTimer +1)
+    },1000)
+  };
+  const stopTimer = ()=>{
+     clearInterval(ref.current);
+  };
+  console.log(ref.current);
   return (
     <div className="App">
-      <header className="App-header">
-        <button>Start</button>
-        <button>Pause</button>
+      <h1>Timer : {timer}</h1>
+        <button onClick={startTimer}>START</button>
+        <button onClick={stopTimer}>STOP</button>
         <button>Reset</button>
-      </header>
     </div>
   );
 }
