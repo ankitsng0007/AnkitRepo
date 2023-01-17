a24d8b86d55362060f6ad05859ebb9c355692496
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-18 04:09:30 +0530

    file Updated

--- RCT-101/Day-9/asgn-3/countdown-timer/src/App.js
+++ RCT-101/Day-9/asgn-3/countdown-timer/src/App.js
@@ -6,21 +6,37 @@ function App() {
   const [timer,setTimer] = useState(0)
   const ref=useRef(null);
 
+  const fixTimeString = (time)=>{
+  return time <10 ? `0${time}`: time;
+  }
+  const formatTimeToString = (time) =>{
+    const seconds = time % 60;
+    const minutes = Math.floor(time/60)%60;
+    const outputString = `00:${fixTimeString(minutes)}:${fixTimeString(seconds)}`;
+    return outputString;
+  }
+
   const startTimer=()=>{
+    if(ref.current !== null)return;
     ref.current = setInterval(()=>{
       setTimer((prevTimer)=>prevTimer +1)
     },1000)
   };
   const stopTimer = ()=>{
      clearInterval(ref.current);
+     ref.current = null;
   };
+  const resetTimer = ()=>{
+    stopTimer();
+    setTimer(0);
+  }
   console.log(ref.current);
   return (
     <div className="App">
-      <h1>Timer : {timer}</h1>
+      <h1>{formatTimeToString(timer)}</h1>
         <button onClick={startTimer}>START</button>
         <button onClick={stopTimer}>STOP</button>
-        <button>Reset</button>
+        <button onClick={resetTimer}>RESET</button>
     </div>
   );
 }
