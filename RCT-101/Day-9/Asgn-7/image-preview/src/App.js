fdd9e4584b7440ff3af0bf0023ad0d877edede80
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-19 20:48:12 +0530

    file added

---
+++ RCT-101/Day-9/Asgn-7/image-preview/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Image Preview Assignment</h1>
+      </header>
+    </div>
+  );
+}
+export default App;
