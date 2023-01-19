15dd752e8b1c05c11ae0a79951ae1e00bca9ada7
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-19 20:01:36 +0530

    file added

--- RCT-101/Day-9/Asgn-4/scroll-container/src/App.js
+++ RCT-101/Day-9/Asgn-4/scroll-container/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Scroll from "./Component/Scroll"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Scroll Container</h1>
+        <Scroll/>
       </header>
     </div>
   );
---
+++ RCT-101/Day-9/Asgn-4/scroll-container/src/Component/Scroll.jsx
@@ -0,0 +1,18 @@
+const style1={
+    width:"300px",
+    height:"50px",
+    border:"solid 1px red",
+    overflow:"auto",
+    color:"red",
+    backgroundColor:"yellow",
+    }
+function Scroll(){
+    
+    return(   
+        <div 
+        style={style1}>
+            <h1>Hello World, I am Ankit Singh Panchshil</h1>
+        </div>
+    )
+}
+export default Scroll;
