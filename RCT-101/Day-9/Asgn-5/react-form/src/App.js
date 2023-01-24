ac5ff16e0cbf83e029cbf4658fd87a4c51ad7b81
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-25 00:43:04 +0530

    file updated

--- RCT-101/Day-9/Asgn-5/react-form/src/App.js
+++ RCT-101/Day-9/Asgn-5/react-form/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import {useRef, useState} from "react";
 const initState={
   name:"",
   email:"",
@@ -12,51 +12,64 @@ const initState={
 function App() {
   const [formData,setFormData] = useState(initState);
   const {name,isMarried,email,country,gender}=formData;
+  const fileRef=useRef();
+  const handleChange= (e)=>{
+    let {name,value,checked,type}=e.target;
+    value=type==="checkbox"?checked : value;
+    setFormData((prev)=>({...prev,[name]:value}))
+  }
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    console.log(formData);
+    console.log(fileRef.current.files[0])
+  }
   return (
      <div className="App">
       <h1>React Form</h1>
       <form onSubmit={()=>{}}>
         <label>
           Name: 
-          <input type="text" name="name" value={name}
+          <input onChange={handleChange} type="text" name="name" value={name}
           placeholder="Enter Name"/>
         </label>
         <br/>
         <br/>
         <label>
           Email: 
-          <input type="email" name="Email" value={email}
+          <input onChange={handleChange} type="email" name="email" value={email}
           placeholder="Enter Email"/>
         </label>
         <br/>
         <br/>
         <label>
           Country: 
-          <input type="text" name="country" value={country}
+          <input onChange={handleChange} type="text" name="country" value={country}
           placeholder="Enter Country"/>
         </label>
         <br/>
         <br/>
         <label>
           Gender: 
-          <select>
-          <option value={M}>Male</option>
-          <option value={F}>Female</option>
-          <option value={O}>Other</option>
+          <select value={gender} onChange={handleChange}>
+          <option value="M">Male</option>
+          <option value="F">Female</option>
+          <option value="O">Other</option>
           </select>
         </label>
         <br/>
         <br/>
         <label>
           IsMarried: 
-          <input type="checkbox" name="isMarried" value={isMarried}/>
+          <input onChange={handleChange} type="checkbox" name="isMarried" value={isMarried}/>
         </label>
         <br/>
         <br/>
-         
-          <input type="submit" />
-        <br/>
-        <br/>
+        <label>
+          Photo: 
+          <input type="file" ref={fileRef}/>
+        </label>
+        <br/><br/>
+        <input type="submit"/>
       </form>
      </div>
   );
