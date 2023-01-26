04ae1dc857b34a79215417c76ff7a34014d81998
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-26 23:54:55 +0530

    file added

--- RCT-101/Day-11/Asgn-2/theme-context/src/App.js
+++ RCT-101/Day-11/Asgn-2/theme-context/src/App.js
@@ -3,11 +3,24 @@ import './App.css';
 import { useContext } from 'react';
 import { ThemeContext } from './Component/ThemeContextProvider';
 
+const style={
+  light:{
+    color:"black",
+    background:"#cecece",
+    padding:"50px"
+},
+dark:{
+  color:"white",
+  background:"black",
+  padding:"50px"
+}
+}
 function App() {
   const value = useContext(ThemeContext);
   console.log(value);
   return (
-    <div className="App">
+    <div className="App"
+    style={value.theme==="dark" ? style.dark:style.light}>
         <h1>Theme is {value.theme}</h1>
         <h3>To See The Magic Click on Button</h3>
         <button onClick={value.toggleTheme}>Change Theme</button>
