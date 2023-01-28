9dac6996e6b661e53f875b20bfc791f5f932c49c
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-28 12:39:29 +0530

    File Updated

--- RCT-101/Day-11/auth-context/src/App.js
+++ RCT-101/Day-11/auth-context/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from "./Component/Form";
 
 function App() {
+  
+  const onSubmit=(e)=>{
+    e.preventDefault();
+    alert("Trying To Submit");
+  };
   return (
     <div className="App">
-      <header className="App-header">
         <h1>Auth Context</h1>
-      </header>
+        <Form onSubmit={onSubmit} title="LOG IN">
+          <input placeholder="Enter Name"/>
+        </Form>
     </div>
   );
 }
