b55a4f2a71763ac576df7186373734f9ede625a9
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-29 17:10:54 +0530

    File Updated

--- RCT-101/Day-12/Asgn-1/react-routing/src/Component/About.jsx
+++ RCT-101/Day-12/Asgn-1/react-routing/src/Component/About.jsx
@@ -1,6 +1,14 @@
+import { Navigate, useNavigate } from "react-router-dom"
 
 export function About(){
+    const navigate = useNavigate();
     return(
+        <div>
         <h3>I am the Best.</h3>
+        <h2>Thank You</h2>
+        <button onClick={()=>{
+            navigate("/product/laptop")
+        }}>Submit</button>
+        </div>
     )
 }
