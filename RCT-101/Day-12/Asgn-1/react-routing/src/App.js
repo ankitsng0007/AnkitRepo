6a956ef2191eff9cd8283c9c61de32e742e27ef8
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-29 16:40:35 +0530

    file added

--- RCT-101/Day-12/Asgn-1/react-routing/src/App.js
+++ RCT-101/Day-12/Asgn-1/react-routing/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Home } from './Component/Home';
 import { About } from './Component/About';
 import { Navbar } from './Component/Navbar';
+import { Product } from './Component/Products';
 import { Routes,Route } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path={'/'} element={<Home/>}></Route>
         <Route path={'/about'} element={<About/>}></Route>
+        <Route path={'/Product/:id'} element={<Product/>} ></Route>
       </Routes>
     </div>
   );
--- RCT-101/Day-12/Asgn-1/react-routing/src/Component/Navbar.jsx
+++ RCT-101/Day-12/Asgn-1/react-routing/src/Component/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 export const Navbar=()=>{
     return(
         <div>
-    <Link>Home</Link>
-    <Link>About</Link>
+    <Link to={"/"}>Home</Link>
+    <Link to={"/about"}>About</Link>
     </div>
     )
 }
