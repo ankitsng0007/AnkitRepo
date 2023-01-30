2e797c6ca32f97afa480042643d3a42eeaffeebf
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-30 10:14:19 +0530

    file added

--- RCT-101/Day-12/ClassWork/navbar-routing/src/Component/Navbar.jsx
+++ RCT-101/Day-12/ClassWork/navbar-routing/src/Component/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
+
+const links=[{path:"/",title:"Home"},
+             {path:"/users",title:"Users"},
+             {path:"/about",title:"About"},
+             {path:"/contact",title:"Contact"}];
 export const Navbar = ()=>{
 return(
     <div style={{
         display:"flex",
         justifyContent:"space-around",
-        width:"80%",
-        margin:"auto"
     }}>
-        <Link to="/">Home</Link>
-        <Link to="/users">users</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+        {links.map((el)=>(
+            <NavLink key={el.path} to={el.path}>{el.title}</NavLink>
+        ))}
     </div>
 )
 }
