d3262e519f7a78381796658d186293d5db4f8f14
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-30 11:48:39 +0530

    file added

--- RCT-101/Day-12/ClassWork/navbar-routing/src/Pages/Users.jsx
+++ RCT-101/Day-12/ClassWork/navbar-routing/src/Pages/Users.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
+const getData=(url)=>{
+    return fetch(url).then((res)=>res.json());
+}
 export const Users =()=>{
+    const [data,setData]=React.useState({});
+    React.useEffect(()=>{
+        getData(`https://reqres.in/api/users/?page=2`).then((res)=>{
+            console.log(res);
+        })
+    },[])
     return(
     <h1>Users</h1>
     )
