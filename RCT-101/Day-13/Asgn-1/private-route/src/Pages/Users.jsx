d9ec96170996711f2d26eeb588b540ac8913bf65
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-30 23:59:30 +0530

    file added

--- RCT-101/Day-13/Asgn-1/private-route/src/Pages/Users.jsx
+++ RCT-101/Day-13/Asgn-1/private-route/src/Pages/Users.jsx
@@ -1,6 +1,28 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
+const getData=(url)=>{
+    return fetch(url).then((res)=>res.json());
+}
 export const Users=()=>{
+    const [data,setData] = React.useState();
+    React.useEffect(()=>{
+      getData(`https://reqres.in/api/users/?page=1`).then((res)=>{
+        console.log(res);
+        setData(res);
+        console.log(data.data);
+      })
+    },[])
     return(
+        <>
         <h1>Users</h1>
+        <ul>
+            {data && data.data && data.data.map((el)=>
+            <div key={el.id} style={{display:"flex",flexDirection:"column",alignItems:"centre"}}>
+              <img src={el.avatar} alt="profile-pic" style={{width:"40%",margin:"auto"}}/>
+              <Link to={`/users/${el.id}`}>More Detail</Link>
+            </div>)}
+        </ul>
+        </>
     );
 };
