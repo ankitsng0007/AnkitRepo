c8582c68deeeb9012390c1a3b1ff4bb3f42758b7
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-30 12:21:43 +0530

    file updated

--- RCT-101/Day-12/ClassWork/navbar-routing/src/Pages/Users.jsx
+++ RCT-101/Day-12/ClassWork/navbar-routing/src/Pages/Users.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const getData=(url)=>{
     return fetch(url).then((res)=>res.json());
@@ -8,9 +9,20 @@ export const Users =()=>{
     React.useEffect(()=>{
         getData(`https://reqres.in/api/users/?page=2`).then((res)=>{
             console.log(res);
+            setData(res);
+            console.log(data)
         })
     },[])
     return(
+        <>
     <h1>Users</h1>
+    <ul>
+    {data && data.data && data.data.map((el)=>
+        <li>
+            <Link to={`/users/${el.id}`}>{el.first_name}</Link>
+            </li>
+    )}
+    </ul>
+    </>
     )
 }
