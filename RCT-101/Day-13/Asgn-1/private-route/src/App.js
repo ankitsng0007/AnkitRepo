b8d30629095bd3771ad48539029b0aa730bf77fb
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-30 23:27:24 +0530

    file added

--- RCT-101/Day-13/Asgn-1/private-route/src/App.js
+++ RCT-101/Day-13/Asgn-1/private-route/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { AllRoutes } from './Pages/AllRoutes';
+import { Navbar } from './Pages/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
       <AllRoutes/>
     </div>
   );
--- RCT-101/Day-13/Asgn-1/private-route/src/Pages/Navbar.jsx
+++ RCT-101/Day-13/Asgn-1/private-route/src/Pages/Navbar.jsx
@@ -1,3 +1,19 @@
 import {NavLink} from "react-router-dom";
 
-const links=[]
+const links=[
+    {path:"/",title:"Home"},
+    {path:"/users",title:"Users"},
+    {path:"/contact",title:"Contact"}
+];
+export const Navbar=()=>{
+    return(<div style={{display:"flex",justifyContent:"space-around"}}>{
+        links.map((el)=>(
+            <NavLink 
+            style={({isActive})=>{
+                return isActive? {color:"red"}:{color:"black"}
+            }} 
+            to={el.path}>{el.title}</NavLink>
+        ))
+    }</div>
+    )
+}
