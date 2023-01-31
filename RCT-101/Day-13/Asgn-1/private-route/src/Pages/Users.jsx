b16fae984a1d0da29daa820721a73729cd6a8319
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-31 20:30:04 +0530

    File Updated

--- RCT-101/Day-13/Asgn-1/private-route/src/Pages/Users.jsx
+++ RCT-101/Day-13/Asgn-1/private-route/src/Pages/Users.jsx
@@ -3,11 +3,23 @@ import {Link ,Navigate,useSearchParams} from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
 const getData=(url)=>{
     return fetch(url).then((res)=>res.json());
-}
+}   
+    const getCurrentPageFromUrl=(value)=>{
+      value=Number(value);
+      if(value==="number" && value<=0){
+       value=1;
+      }
+      if(!value){
+        value=1
+      }
+      return value;
+    };
 export const Users=()=>{
     const [data,setData] = React.useState();
-    const [page,setPage] = React.useState(1);
     const [searchParams,setSearchParams] = useSearchParams();
+    const initPage = getCurrentPageFromUrl(searchParams.get("page"))
+    const [page,setPage] = React.useState(initPage);
+    const [text,setText] = React.useState("");
     React.useEffect(()=>{
       getData(`https://reqres.in/api/users/?page=${page}`).then((res)=>{
         console.log(res);
@@ -16,11 +28,12 @@ export const Users=()=>{
     },[page])
 
     React.useEffect(()=>{
-      setSearchParams({page})
-    },[page])
+      setSearchParams({page,text})
+    },[page,text])
     return(
         <>
         <h1>Users</h1>
+        <input placeholder="Search" type="text" value={text} onChange={(e)=>setText(e.target.value)}/>
         <ul>
             {data && data.data && data.data.map((el)=>
             <div key={el.id} style={{display:"flex",flexDirection:"column",alignItems:"centre"}}>
