3e80b4b6ba0a67f331bc9be98cd1d1ced3cff847
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-31 11:06:14 +0530

    file added

--- RCT-101/Day-13/Asgn-1/private-route/src/Pages/AllRoutes.jsx
+++ RCT-101/Day-13/Asgn-1/private-route/src/Pages/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import {Route,Routes} from "react-router-dom";
 import { Contact } from "./Contact";
 import { Home } from "./Home";
+import { PageNotFound } from "./PageNotFound";
 import { SingleUserPage } from "./SingleUserPage";
 import { Users } from "./Users";
 
@@ -11,6 +12,7 @@ export const AllRoutes=()=>{
             <Route path="/users" element={<Users/>}></Route>
             <Route path="/contact" element={<Contact/>}></Route>
             <Route path="/users/:id" element={<SingleUserPage/>}></Route>
+            <Route path="*" element={<PageNotFound/>}></Route>
         </Routes>
     )
 }
