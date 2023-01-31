cf286a609ee6ea0e57be6335b22296c7e9b9ffb3
ankitsng0007 <ankitsng0007@gmail.com>
2023-01-31 10:10:06 +0530

    file added

--- RCT-101/Day-13/Asgn-1/private-route/src/Pages/AllRoutes.jsx
+++ RCT-101/Day-13/Asgn-1/private-route/src/Pages/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import {Route,Routes} from "react-router-dom";
 import { Contact } from "./Contact";
 import { Home } from "./Home";
+import { SingleUserPage } from "./SingleUserPage";
 import { Users } from "./Users";
 
 export const AllRoutes=()=>{
@@ -9,6 +10,7 @@ export const AllRoutes=()=>{
             <Route path="/" element={<Home/>}></Route>
             <Route path="/users" element={<Users/>}></Route>
             <Route path="/contact" element={<Contact/>}></Route>
+            <Route path="/users/:id" element={<SingleUserPage/>}></Route>
         </Routes>
     )
 }
--- RCT-101/Day-13/Asgn-1/private-route/src/Pages/SingleUserPage.jsx
+++ RCT-101/Day-13/Asgn-1/private-route/src/Pages/SingleUserPage.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import {useParams} from "react-router-dom";
+
+const getData=(url)=>{
+    return fetch(url).then((res)=>res.json())
+};
 export const  SingleUserPage=()=>{
+    const {id} = useParams();
+    const [userDetail,setUserDetail] = React.useState();
+    React.useEffect(()=>{
+        getData(`https://reqres.in/api/users/${id}`).then((res)=>
+          setUserDetail(res.data)
+        );
+    },[])
+    console.log(userDetail);
     return(
-        <h1>User Detail</h1>
+        <>
+        <img src={userDetail.avatar} alt="profile-pic" />
+        <h1>User : </h1>
+        </>
     );
 };
