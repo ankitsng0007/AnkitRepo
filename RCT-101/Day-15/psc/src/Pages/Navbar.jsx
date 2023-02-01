17e9d357ec59295fc46941fc8ca5bff493ec01c5
ankitsng0007 <ankitsng0007@gmail.com>
2023-02-01 11:25:13 +0530

    file added

--- RCT-101/Day-15/psc/src/Pages/Navbar.jsx
+++ RCT-101/Day-15/psc/src/Pages/Navbar.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
+import { Home } from "./Home";
+import { Products } from "./Products";
+import { Contact } from "./Contact";
 
+const links=[{path:"/",title:"<Home/>"},
+             {path:"/products",title:"<Products/>"},
+             {path:"/contact",title:"<Contact/>"},
+             {path:"/*",title:"<NotFound/>"}]
 export const Navbar=()=>{
     return(
       <>
