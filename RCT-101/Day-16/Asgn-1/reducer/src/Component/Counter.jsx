987816e3541f14f062aeaa0220d3dab81ddc5e4f
ankitsng0007 <ankitsng0007@gmail.com>
2023-02-03 23:56:58 +0530

    file added

--- RCT-101/Day-16/Asgn-1/reducer/src/Component/Counter.jsx
+++ RCT-101/Day-16/Asgn-1/reducer/src/Component/Counter.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 export const Counter=()=>{
     const [count,setCount]=React.useState(0)
+    const handleIncrement=()=>{
+        setCount(count+1);
+    }
+    const handleDecrement=()=>{
+        setCount(count-1);
+    }
    return(
     <div>
         <h1>Counter : {count}</h1>
-        <button>Inc</button>
-        <button>Dec</button>
+        <button onClick={handleIncrement}>Inc</button>
+        <button onClick={handleDecrement}>Dec</button>
     </div>
    )
 }
