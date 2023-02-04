590c4e179e24f36216bcd45da55b72465093492f
ankitsng0007 <ankitsng0007@gmail.com>
2023-02-04 18:19:37 +0530

    file added

--- RCT-101/Day-16/Asgn-1/reducer/src/Component/Reducer.jsx
+++ RCT-101/Day-16/Asgn-1/reducer/src/Component/Reducer.jsx
@@ -1,12 +1,18 @@
 export const Reducer=(state,action)=>{
-    if(action.type==="IncrementCount"){
-      return state +1;
+  //  if(action.type==="IncrementCount"){
+  //    return state +1;
+  //  }
+  //  if(action.type==="DecrementCount"){
+//  return state - 1;
+  //  }
+  //  if(action.type==="ResetCount"){
+   //     return 0;
+  //  }
+    //  return state;
+
+    switch(action.type){
+        case "IncrementCount": return state+1;
+        case "DecrementCount": return state-1;
+        case "ResetCount": return 0;
     }
-    if(action.type==="DecrementCount"){
-      return state - 1;
-    }
-    if(action.type==="ResetCount"){
-        return 0;
-    }
-      return state;
   }
