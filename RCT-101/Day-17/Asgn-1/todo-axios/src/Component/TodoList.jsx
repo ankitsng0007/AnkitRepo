2ee777736da46aa8587458d1aa447114a37df6eb
ankitsng0007 <ankitsng0007@gmail.com>
2023-02-07 16:15:46 +0530

    file added

--- RCT-101/Day-17/Asgn-1/todo-axios/src/Component/TodoList.jsx
+++ RCT-101/Day-17/Asgn-1/todo-axios/src/Component/TodoList.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export const TodoList=({title,status,id,handleToggle,handleDelete})=>{
     return(
-        <div style={{
+        <ul style={{
             display:"flex",
             gap:"1rem",
-            justifyContent:"space-between"
+            justifyContent:"space-around"
         }}>
             <b>{title}</b> {status? "Done":"Not Done"}
             <button onClick={()=>handleToggle(id,!status)}>Toggle Status</button>
             <button onClick={()=>handleDelete(id)}>Delete</button>
-        </div>
+        </ul>
      )
 }
