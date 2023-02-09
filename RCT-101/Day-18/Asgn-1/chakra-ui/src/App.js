ce8494b6853168d7ea7d6741e7b7979868628c56
ankitsng0007 <ankitsng0007@gmail.com>
2023-02-09 10:14:39 +0530

    file added

--- RCT-101/Day-18/Asgn-1/chakra-ui/src/App.js
+++ RCT-101/Day-18/Asgn-1/chakra-ui/src/App.js
@@ -5,10 +5,8 @@ import { Form } from './Component/Form';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
          <h1>Chakra UI</h1>
          <Form/>
-      </header>
     </div>
   );
 }
--- RCT-101/Day-18/Asgn-1/chakra-ui/src/Component/Form.jsx
+++ RCT-101/Day-18/Asgn-1/chakra-ui/src/Component/Form.jsx
@@ -1,7 +1,18 @@
-import {Box,Input,Button, Heading} from "@chakra-ui/react";
+import {Box,Input,Button, Heading, Container, Stack} from "@chakra-ui/react";
 
 export const Form=()=>{
     return(
+        <Container>
         <Heading>Form</Heading>
+        <Stack gap="1rem">
+        <Box>
+        <Input type="email" placeholder="Enter Email Adress"/>
+        <Input type="password" placeholder="Enter Password"/>
+        </Box>
+        <Box>
+            <Button>Sign Up</Button>
+        </Box>
+        </Stack>
+        </Container>
     )
 }
