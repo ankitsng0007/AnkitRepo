772b43510dbf465634de7d817810d153bda5e019
ankitsng0007 <ankitsng0007@gmail.com>
2023-02-09 10:07:16 +0530

    file added

--- RCT-101/Day-18/Asgn-1/chakra-ui/src/App.js
+++ RCT-101/Day-18/Asgn-1/chakra-ui/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Form } from './Component/Form';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
          <h1>Chakra UI</h1>
+         <Form/>
       </header>
     </div>
   );
---
+++ RCT-101/Day-18/Asgn-1/chakra-ui/src/Component/Form.jsx
@@ -0,0 +1,7 @@
+import {Box,Input,Button, Heading} from "@chakra-ui/react";
+
+export const Form=()=>{
+    return(
+        <Heading>Form</Heading>
+    )
+}
