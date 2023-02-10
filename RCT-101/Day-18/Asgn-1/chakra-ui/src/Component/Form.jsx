bc0062b44c65d163aa164402c1f2001ad5e47091
ankitsng0007 <ankitsng0007@gmail.com>
2023-02-10 23:58:23 +0530

    file added

--- RCT-101/Day-18/Asgn-1/chakra-ui/src/Component/Form.jsx
+++ RCT-101/Day-18/Asgn-1/chakra-ui/src/Component/Form.jsx
@@ -43,7 +43,7 @@ export const Form=()=>{
         </box>
         <Box>
             <Menu>
-            <MenuButton as={Button}>
+            <MenuButton as={Button} rightIcon={<chevronDownIcon />}>
                 Actions
             </MenuButton>
             <MenuList>
