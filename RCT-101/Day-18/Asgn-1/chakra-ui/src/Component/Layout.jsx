e2695adce8c4f53078c7bb430498c662bf3ee1ed
ankitsng0007 <ankitsng0007@gmail.com>
2023-02-11 23:52:03 +0530

    file added

--- RCT-101/Day-18/Asgn-1/chakra-ui/src/Component/Layout.jsx
+++ RCT-101/Day-18/Asgn-1/chakra-ui/src/Component/Layout.jsx
@@ -1,8 +1,16 @@
-import { Box } from "@chakra-ui/react"
+import { Box, SimpleGrid } from "@chakra-ui/react"
 export const Layout=()=>{
     return(
         <Box>
-            
+            <SimpleGrid columns={4} spacing={10}>
+                <Box bg="red" height={10}></Box>
+                <Box bg="red" height={10}></Box>
+                <Box bg="red" height={10}></Box>
+                <Box bg="red" height={10}></Box>
+                <Box bg="red" height={10}></Box>
+                <Box bg="red" height={10}></Box>
+                <Box bg="red" height={10}></Box>
+            </SimpleGrid>
         </Box>
     )
 }
