dd1a09826d4df5f420a8d7d65cd45f886afa2338
ankitsng0007 <ankitsng0007@gmail.com>
2023-02-12 10:42:43 +0530

    file added

--- RCT-101/Day-18/Asgn-1/chakra-ui/src/Component/BasicPortal.jsx
+++ RCT-101/Day-18/Asgn-1/chakra-ui/src/Component/BasicPortal.jsx
@@ -1,13 +1,33 @@
-import { AlertDialog, Button, useDisclosure } from "@chakra-ui/react";
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, useDisclosure } from "@chakra-ui/react";
 import React from "react";
 
 export const BasicPortal=()=>{
     const {isOpen,onClose,onOpen}=useDisclosure();
     return(
         <>
-        <Button>
+        <Button colorScheme="red" onClick={onOpen}>
             Clicking on This opens the Alert Dialog
         </Button>
+        <AlertDialog>
+            <AlertDialogOverlay>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        This ia Title when alert dailog opens
+                    </AlertDialogHeader>
+                    <AlertDialogBody>
+                        Alert Dailog Body comes Here.
+                    </AlertDialogBody>
+                    <AlertDialogFooter>
+                        <Button>
+                            Cancel
+                        </Button>
+                        <Button>
+                            Delete
+                        </Button>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialogOverlay>
+        </AlertDialog>
         </>
     )
 }
