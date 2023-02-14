1e58cb902ef37b2d987fd7c2d48740f00950d43f
ankitsng0007 <ankitsng0007@gmail.com>
2023-02-15 00:07:13 +0530

    file added

--- RCT-101/Day-19/Asgn-1/e-comerse/src/Component/Listing.jsx
+++ RCT-101/Day-19/Asgn-1/e-comerse/src/Component/Listing.jsx
@@ -1,6 +1,14 @@
 import { Button } from "@chakra-ui/react"
+import axios, {Axios} from "axios";
+import { useEffect, useState } from "react";
 
 export const Listing=()=>{
+    const [data,setData]=useState([]);
+    useEffect(()=>{
+        axios.get("http://localhost:8080/items")
+        .then(res=>setData(res.data))
+    },[])
+    console.log(data);
     return(
         <>
         <Button variant={["sm","base","md"]}>Click Me</Button>
