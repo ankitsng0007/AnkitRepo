43c10abdfead7b72c4fe2ced46c5c7a15d9323cc
ankitsng0007 <ankitsng0007@gmail.com>
2023-02-15 00:19:32 +0530

    file added

--- RCT-101/Day-19/Asgn-1/e-comerse/src/Component/Listing.jsx
+++ RCT-101/Day-19/Asgn-1/e-comerse/src/Component/Listing.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@chakra-ui/react"
+import { Box, Button, Img, SimpleGrid } from "@chakra-ui/react"
 import axios, {Axios} from "axios";
 import { useEffect, useState } from "react";
 
@@ -11,7 +11,19 @@ export const Listing=()=>{
     console.log(data);
     return(
         <>
-        <Button variant={["sm","base","md"]}>Click Me</Button>
+        {/*<Button variant={["sm","base","md"]}>Click Me</Button>*/}
+        <SimpleGrid columns="2" spacing={5}>
+            {
+                data && data.map((el)=>{
+                    return(
+                        <Box key={el.id}>
+                            <Img src="el.imageURL" boxSize="100px"/>
+                            {el.name}
+                        </Box>
+                    )
+                })
+            }
+        </SimpleGrid>
         </>
     )
 }
