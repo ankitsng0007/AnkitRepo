264bdab0baaa65e311cb0c8d5b8ca4e01417cbb9
ankitsng0007 <ankitsng0007@gmail.com>
2023-02-28 20:51:03 +0530

    file added

--- RCT-101/Day-20/PSC/create-profile/src/App.js
+++ RCT-101/Day-20/PSC/create-profile/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route,Routes} from "react-router-dom";
+import { Home } from './Pages/Home';
+import { Example } from './Pages/Example';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Create Your Profile</h1>
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/dashboard' element={<Example/>}></Route>
+    </Routes>
+    </>
   );
 }
 
