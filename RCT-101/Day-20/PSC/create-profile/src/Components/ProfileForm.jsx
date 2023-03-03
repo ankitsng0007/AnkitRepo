ea6365294bca76b6a260312c6631971b584ee88c
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-03 20:29:43 +0530

    file added

--- RCT-101/Day-20/PSC/create-profile/src/Components/ProfileForm.jsx
+++ RCT-101/Day-20/PSC/create-profile/src/Components/ProfileForm.jsx
@@ -1,7 +1,13 @@
+import { VStack } from "@chakra-ui/react";
 import React from "react";
 
 export const ProfileForm=()=>{
     return(
-        <></>
+        <VStack
+        h="full"
+        w="full"
+        p={10}
+        alignItems="flex-start"
+        bg="orange.300"></VStack>
     )
 }
--- RCT-101/Day-20/PSC/create-profile/src/Pages/Home.jsx
+++ RCT-101/Day-20/PSC/create-profile/src/Pages/Home.jsx
@@ -1,7 +1,14 @@
+import { Container, Flex } from "@chakra-ui/react";
 import React from "react";
+import { Profile } from "../Components/Profile";
+import { ProfileForm } from "../Components/ProfileForm";
 
 export const Home =()=>{
     return(
-        <h1>Home</h1>
+        <Container maxW="container.xl" p={0}>
+            <Flex h="100vh" py={20}></Flex>
+             <ProfileForm />
+             <Profile />
+        </Container>
     )
 }
