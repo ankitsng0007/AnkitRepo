41e2bbc76890f0f2c30c8c27eb79ad9169c1d3de
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-09 16:18:40 +0530

    file added

--- RCT-101/Day-20/PSC/create-profile/src/Pages/Home.jsx
+++ RCT-101/Day-20/PSC/create-profile/src/Pages/Home.jsx
@@ -5,8 +5,10 @@ import { ProfileForm } from "../Components/ProfileForm";
 
 export const Home =()=>{
     return(
-        <Container maxWidth={{base:"full", md:"container.xl"}} p={0}>
-            <Flex h="200vh" w="full" py={10}>
+        <Container 
+        maxWidth={{base:"full", md:"container.xl"}} 
+        p={{base:2, md:0}}>
+            <Flex h="250vh" w="full" py={10} direction={{base:"column",md:"row"}}>
             <ProfileForm />
              <Profile />
             </Flex>
