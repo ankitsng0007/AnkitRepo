9fb6990d23d10449ee1f530b5009ccc1d17ee7ef
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-10 15:44:32 +0530

    file added

--- RCT-101/Day-20/PSC/create-profile/src/Pages/Products.jsx
+++ RCT-101/Day-20/PSC/create-profile/src/Pages/Products.jsx
@@ -1,18 +1,34 @@
-import { Container } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+import { Box, Container, Grid, GridItem, Image, VStack } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const getData=()=>{
     return axios.get(`http://localhost:8080/products`);
 }
 export const Products =()=>{
+    const [data,setData] = useState([]);
+
     useEffect(()=>{
-        getData().then((res)=>console.log(res.data))
+        getData().then((res)=>setData(res.data))
     },[])
+    console.log(data);
     return(
         <Container maxW={{base:"full",lg:"container.xl"}}
         p={{base:2,lg:0}}>
-
+        <Grid templateColumns={{ base:1, md:2, lg:3 }}>
+          {data && data.map((el)=>(
+            <GridItem key={el.id}>
+                <VStack>
+                  <Box>
+                    <Image src={el.image} alt="product-img"/>
+                  </Box>
+                  <Box>
+                    {el.title}
+                  </Box>
+                </VStack>
+            </GridItem>
+          ))}
+        </Grid>
         </Container>
     )
 }
