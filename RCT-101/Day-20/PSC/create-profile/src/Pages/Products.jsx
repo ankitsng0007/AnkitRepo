4f9e0bc53398b002749e289fd89fbdb228f938da
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-10 15:15:41 +0530

    axios

--- RCT-101/Day-20/PSC/create-profile/src/Pages/Products.jsx
+++ RCT-101/Day-20/PSC/create-profile/src/Pages/Products.jsx
@@ -1,7 +1,14 @@
 import { Container } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
+const getData=()=>{
+    return axios.get(`http://localhost:8080/products`);
+}
 export const Products =()=>{
+    useEffect(()=>{
+        getData().then((res)=>console.log(res.data))
+    },[])
     return(
         <Container maxW={{base:"full",lg:"container.xl"}}
         p={{base:2,lg:0}}>
