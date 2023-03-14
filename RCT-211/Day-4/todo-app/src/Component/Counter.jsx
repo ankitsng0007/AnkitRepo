e88a50866a860db9fb86d82d7e32bbe2b38b0949
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-14 10:46:05 +0530

    action added

--- RCT-211/Day-4/todo-app/src/Component/Counter.jsx
+++ RCT-211/Day-4/todo-app/src/Component/Counter.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { handleAdd, handleReduce } from '../Redux/action';
 
 function Counter() {
     const count = useSelector((reduxStore) => reduxStore.count);
+    const dispatch = useDispatch();
+
+    const addHandle=()=>{
+        dispatch(handleAdd(1));
+    }
+    const reduceHandle= () =>{
+        dispatch(handleReduce(2));
+    }
   return (
     <div>
       <h1>Count : {count}</h1>
-      <button>ADD</button>
-      <button>REDUCE</button>
+      <button onClick={addHandle}>ADD</button>
+      <button onClick={reduceHandle}>REDUCE</button>
     </div>
   );
 }
