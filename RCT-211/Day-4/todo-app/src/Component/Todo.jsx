4f6b92f2feb52822a0f0fb97e9a86251095efb4f
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-15 23:22:02 +0530

    todo added

--- RCT-211/Day-4/todo-app/src/Component/Todo.jsx
+++ RCT-211/Day-4/todo-app/src/Component/Todo.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React ,{useEffect}from 'react';
 import TodoInput from './TodoInput';
 
 function Todo() {
     const getTodos=()=>{
 
     }
-    
+    useEffect(()=>{
+      getTodos();
+    },[])
   return (
     <div>
       <h1>TODO</h1>
