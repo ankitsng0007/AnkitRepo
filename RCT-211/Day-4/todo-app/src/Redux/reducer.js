60d1b8317d8523dd30846bc32e93d6697912ffc7
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-16 10:06:22 +0530

    reducer updated

--- RCT-211/Day-4/todo-app/src/Redux/reducer.js
+++ RCT-211/Day-4/todo-app/src/Redux/reducer.js
@@ -10,7 +10,13 @@ const reducer =(oldState = initState,action)=>{
             return{...oldState, count: oldState.count+payload};
         case types.REDUCE:
             return{...oldState,count : oldState.count-payload};
-        
+        case types.GET_TODOS_REQUEST:
+            return{...oldState,isLoading:true };
+        case types.GET_TODOS_SUCCESS:
+            return{...oldState,isLoading:false,todos:payload };
+        case types.GET_TODOS_ERROR:
+            return{...oldState,isLoading:false,isError:true };
+
             default:
             return oldState;
     }
