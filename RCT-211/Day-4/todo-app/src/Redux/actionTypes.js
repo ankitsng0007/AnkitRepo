d9a8462f485f3934aa3299ec6cb47f3a70b9cffb
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-16 09:40:36 +0530

    action updated

--- RCT-211/Day-4/todo-app/src/Redux/actionTypes.js
+++ RCT-211/Day-4/todo-app/src/Redux/actionTypes.js
@@ -1,3 +1,7 @@
 
 export const ADD ="ADD";
 export const REDUCE="REDUCE";
+
+export const GET_TODOS_REQUEST="GET_TODOS_REQUEST";
+export const GET_TODOS_SUCCESS="GET_TODOS_SUCCESS";
+export const GET_TODOS_ERROR="GET_TODOS_ERROR";
