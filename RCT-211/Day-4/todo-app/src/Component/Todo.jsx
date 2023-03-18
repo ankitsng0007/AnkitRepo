983674ab137cd714d8862acf98323a2e8a516eb6
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-18 10:52:24 +0530

    post added

--- RCT-211/Day-4/todo-app/src/Component/Todo.jsx
+++ RCT-211/Day-4/todo-app/src/Component/Todo.jsx
@@ -2,8 +2,9 @@ import React ,{useEffect}from 'react';
 import TodoInput from './TodoInput';
 import axios from "axios";
 import { useDispatch, useSelector } from 'react-redux';
-import { getTodosError, getTodosRequest, getTodosSuccess } from '../Redux/action';
+import { getTodosError, getTodosRequest, getTodosSuccess, postTodosError, postTodosRequest, postTodosSuccess } from '../Redux/action';
 import { store } from '../Redux/store';
+import { POST_TODOS_ERROR } from '../Redux/actionTypes';
 
 function Todo() {
     const dispatch = useDispatch();
@@ -25,7 +26,14 @@ function Todo() {
           title,
           status:false
         }
-        dispatch(postTodoRequest())
+        dispatch(postTodosRequest());
+        axios
+          .post("http://localhost:8080/todos",payload)
+          .then((res)=>{
+            dispatch(postTodosSuccess(res.data));
+          }).catch((err)=>{
+            dispatch(postTodosError)
+          })
       }
     }
     useEffect(()=>{
@@ -34,7 +42,7 @@ function Todo() {
   return (
     <div>
       <h1>TODO</h1>
-      <TodoInput/>
+      <TodoInput addTodo={addTodo}/>
       {todos.length>0 && 
        todos.map((el)=>{
         return(
