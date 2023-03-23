e460ff8b00c43a4f9b29bb0f7b2a6454f1df425e
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-23 23:57:49 +0530

    app updated

--- RCT-211/Day-4/todo-app/src/App.js
+++ RCT-211/Day-4/todo-app/src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './Component/Counter';
 import Todo from './Component/Todo';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const isAuth = useSelector((store)=>store.AuthReducer.isAuth)
   return (
     <div>
       <Counter/>
