62706e6661b418952e16b0b0cc4a9b143c5887e8
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-30 21:00:41 +0530

    file added

---
+++ RCT-211/Day-4/todo-app/src/Redux/CounterReducer/reducer.js
@@ -0,0 +1,17 @@
+import * as types from "./actionTypes";
+
+const initState={ count:20, todos:[], isLoading:false, isError:false }
+
+const reducer =(oldState = initState,action)=>{
+    const {type,payload}=action;
+    
+    switch(type){
+        case types.ADD:
+            return{...oldState, count: oldState.count+payload};
+        case types.REDUCE:
+            return{...oldState,count : oldState.count-payload};
+            default:
+            return oldState;
+    }
+};
+export {reducer}
