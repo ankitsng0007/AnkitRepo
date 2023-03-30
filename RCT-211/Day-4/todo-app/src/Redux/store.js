796027afc23e177e77474adeee82cbc10bc11482
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-30 23:58:41 +0530

    file added

--- RCT-211/Day-4/todo-app/src/Redux/store.js
+++ RCT-211/Day-4/todo-app/src/Redux/store.js
@@ -5,7 +5,7 @@ import {reducer as AuthReducer} from "./AuthReducer/reducer";
 
 const logger1=(store)=>(next)=>(action)=>{
     console.log("Next" ,action)
-    return next(action);
+    return next(action); //go to the next middleware if there is any,else go to the reducer function.
 }
 const rootReducer = combineReducers({CounterReducer, TodoReducer,AuthReducer});
 const store = legacy_createStore(rootReducer,applyMiddleware(logger1));
