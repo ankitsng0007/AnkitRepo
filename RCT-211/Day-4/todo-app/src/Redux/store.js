583aad79e809372a6d41fdbf8a66dfdc84477b7b
ankitsng0007 <ankitsng0007@gmail.com>
2023-03-30 23:53:29 +0530

    file added

--- RCT-211/Day-4/todo-app/src/Redux/store.js
+++ RCT-211/Day-4/todo-app/src/Redux/store.js
@@ -1,8 +1,11 @@
-import { combineReducers, legacy_createStore } from "redux";
+import { combineReducers, legacy_createStore,applyMiddleware } from "redux";
 import { reducer as CounterReducer } from "./CounterReducer/reducer";
 import { reducer as TodoReducer } from "./TodoReducer/reducer";
 import {reducer as AuthReducer} from "./AuthReducer/reducer";
 
+const logger1=(store)=>(next)=>(action)=>{
+    return next(action);
+}
 const rootReducer = combineReducers({CounterReducer, TodoReducer,AuthReducer});
 const store = legacy_createStore(rootReducer);
 
