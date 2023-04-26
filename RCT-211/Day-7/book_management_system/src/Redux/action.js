1407b48937f773f1457cefd368d348ed1963287a
ankitsng0007 <ankitsng0007@gmail.com>
2023-04-26 08:52:52 +0530

    file added

--- RCT-211/Day-7/book_management_system/src/Redux/action.js
+++ RCT-211/Day-7/book_management_system/src/Redux/action.js
@@ -5,13 +5,26 @@ const getBookRequest=()=>{
         type: types.GET_BOOKS_REQEST
     }
 }
-const getBookSuccess=()=>{
+const getBookSuccess=(payload)=>{
     return{
-        type: types.GET_BOOKS_SUCCESS
+        type: types.GET_BOOKS_SUCCESS,
+        payload
     }
 }
 const getBookError=()=>{
     return{
         type: types.GET_BOOKS_ERROR
     }
-}
+};
+const getBooks = ()=>
+    (dispatch)=>{
+        dispatch(getBookRequest());
+        return axios.get("http://localhost:8080/books")
+        .then((r)=>{
+            dispatch(getBookSuccess(r.data));
+        })
+        .catch((e)=>{
+            dispatch(getBookError());
+        });
+    };
+export {getBooks};
