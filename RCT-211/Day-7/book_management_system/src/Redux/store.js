886c3b21609c74b11b194378ae9cff1905b0f213
ankitsng0007 <ankitsng0007@gmail.com>
2023-04-26 11:36:45 +0530

    index updated

---
+++ RCT-211/Day-7/book_management_system/src/Redux/store.js
@@ -0,0 +1,11 @@
+import {legacy_createStore,compose, applyMiddleware} from "redux";
+import { reducer } from "./reducer";
+import thunk from "redux-thunk";
+
+const composeEnhancer= window.
+__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose;
+const store = legacy_createStore(
+    reducer,
+    composeEnhancer(applyMiddleware(thunk))
+);
+export {store};
