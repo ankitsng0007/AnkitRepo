7762d5e3f109341206a2b72db47850d92e70495c
ankitsng0007 <ankitsng0007@gmail.com>
2023-04-26 11:55:02 +0530

    index updated

---
+++ RCT-211/Day-7/book_management_system/src/Pages/MainRoutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Routes,Route} from "react-router-dom";
+function MainRoutes() {
+  return (
+    <Routes>
+        <Route path='/' element={<Books />} />
+        <Route path='/books/:id' element={<SingleBook />} />
+        <Route path='/books/:id/edit' element={<EditBook />} />
+        <Route path='/login' element={<LogIn />} />
+        <Route path='*' element={<h3>Page Not Found</h3>} />
+    </Routes>
+  );
+}
+
+export default MainRoutes;
