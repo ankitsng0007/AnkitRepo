987ae1c25ce97c2247e6063816cce1368188a072
ankitsng0007 <ankitsng0007@gmail.com>
2023-05-05 10:59:22 +0530

    imported

--- RCT-211/Day-7/book_management_system/src/App.js
+++ RCT-211/Day-7/book_management_system/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MainRoutes from './Pages/MainRoutes';
 
 function App() {
   return (
     <div className="App">
       <h1>Book Management System</h1>
+      <MainRoutes />
     </div>
   );
 }
