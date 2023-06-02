8d2919e695af7473c7f37eb8cc2a8322afec710b
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-02 11:11:31 +0530

    style added

--- RCT-211/Day-7/book_management_system/src/Pages/Books.jsx
+++ RCT-211/Day-7/book_management_system/src/Pages/Books.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import FilterComp from '../Components/FilterComp';
 import BookList from '../Components/BookList';
+import styled from "styled-components";
 
 function Books() {
   return (
     <div>
+      <BookPageWrapper>
+      <FilterWraper>
       <FilterComp />
+      </FilterWraper>
+
+      <BookWraper>
       <BookList />
+      </BookWraper>
+      </BookPageWrapper>
     </div>
   );
 }
 
+const BookPageWrapper = styled.div`
+display: flex;
+border: 1px solid red;
+`;
+const FilterWraper =styled.div`
+width: 300px;
+border: 2px solid blue;
+`;
+const BookWraper= styled.div`
+width : 100%;
+`;
 export default Books;
