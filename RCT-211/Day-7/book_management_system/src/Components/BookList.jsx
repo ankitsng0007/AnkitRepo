80fd731935a9b6560cffa4b5587124cdc5de48e3
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-02 12:37:55 +0530

    style added

--- RCT-211/Day-7/book_management_system/src/Components/BookList.jsx
+++ RCT-211/Day-7/book_management_system/src/Components/BookList.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import {useSelector,useDispatch} from "react-redux";
 function BookList() {
+  const books = useSelector((store)=>store.books);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+
+  },[]);
   return (
     <div>
       BookList
