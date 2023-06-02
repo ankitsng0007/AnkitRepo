1f48820eb77e4916a8c9ed4b77ad3d2f51d079c3
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-02 10:07:12 +0530

    file added

---
+++ RCT-211/Day-7/book_management_system/src/Pages/Books.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Books() {
+  return (
+    <div>
+      <FilterComp />
+      <BookList />
+    </div>
+  );
+}
+
+export default Books;
--- RCT-211/Day-7/book_management_system/src/Pages/MainRoutes.jsx
+++ RCT-211/Day-7/book_management_system/src/Pages/MainRoutes.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import {Routes,Route} from "react-router-dom";
+import Books from './Books';
+import SingleBook from './SingleBook';
+import EditBook from './EditBook';
+import LogIn from './LogIn';
+
 function MainRoutes() {
   return (
     <Routes>
