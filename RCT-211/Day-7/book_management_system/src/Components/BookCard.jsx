20502f52ece1696dce58fc6cfa0e194f75473f52
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-03 09:44:31 +0530

    File added

--- RCT-211/Day-7/book_management_system/src/Components/BookCard.jsx
+++ RCT-211/Day-7/book_management_system/src/Components/BookCard.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const BookCard = () => {
+const BookCard = ({bookData}) => {
   return (
     <div>
-      
+      <div>
+        <img src='' 
+        alt='Book Cover'
+        width="100%"/>
+      </div>
+      <div>{bookData.book_name}</div>
+      <div>{bookData.category}</div>
+      <div>{bookData.release_year}</div>
     </div>
   );
 }
