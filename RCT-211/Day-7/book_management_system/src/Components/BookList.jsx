cce9a2bca88a87ff67cc226b3f2c5dfb8aa6617a
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-04 19:33:33 +0530

    file added

--- RCT-211/Day-7/book_management_system/src/Components/BookList.jsx
+++ RCT-211/Day-7/book_management_system/src/Components/BookList.jsx
@@ -3,17 +3,26 @@ import {useSelector,useDispatch} from "react-redux";
 import {getBooks} from "../Redux/action";
 import BookCard from './BookCard';
 import styled from 'styled-components';
+import { useLocation } from 'react-router';
+import { useSearchParams } from 'react-router-dom';
 
 const BookList = () => {
   const books = useSelector((store)=>store.books);
   const dispatch = useDispatch();
+  const location = useLocation();
+  const [searchParams] = useSearchParams();
   
   useEffect(()=>{
     //if i don't ave any books in redux then make the API call
-    if(books.length===0){
+    if(location || books.length===0){
+      const getBooksParams = {
+        params : {
+          category:searchParams.getAll("category")
+        }
+      }
       dispatch(getBooks());
     }
-  },[books.length,dispatch]);
+  },[books.length,dispatch, location.search]);
   return (
     <>
       {books.length > 0 &&
