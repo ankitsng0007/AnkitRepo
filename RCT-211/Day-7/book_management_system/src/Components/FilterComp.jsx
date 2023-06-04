eb37149f1ffd1c6742736f05934a9b8cb1c4f085
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-04 14:10:39 +0530

    file added

--- RCT-211/Day-7/book_management_system/src/Components/FilterComp.jsx
+++ RCT-211/Day-7/book_management_system/src/Components/FilterComp.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
 function FilterComp() {
+  const handleFilterCheckbox =()=>{
+
+  }
   return (
     <div>
-      FilterComp
+      <h3>Filter Component</h3>
+      <div>
+        <input type='checkbox'value='Novel' onChange={handleFilterCheckbox}/>
+        <label>Novel</label>
+      </div>
+      <div>
+        <input type='checkbox' value="Motivational" onChange={handleFilterCheckbox}/>
+        <label>Motivational</label>
+      </div>
+      <div>
+        <input type='checkbox' value="Science" onChange={handleFilterCheckbox}/>
+        <label>Science</label>
+      </div>
     </div>
   );
 }
