207f04fe8016a9bcb37781033e55c8b5d125e7be
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-04 16:58:14 +0530

    file added

--- RCT-211/Day-7/book_management_system/src/Components/FilterComp.jsx
+++ RCT-211/Day-7/book_management_system/src/Components/FilterComp.jsx
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useSearchParams} from 'react';
 
 function FilterComp() {
+  const [searchParams,setSearchParams] = useSearchParams()
   const [category,setCategory] = useState([]);
-  const handleFilterCheckbox =()=>{
+  const handleFilterCheckbox =(e)=>{
      //check if data is present in the category.
-
+       const newCategories = [...category];
      //if yes,then remove it (the user has unchecked the checkbox)
-
+       if (newCategories.includes(e.target.value)){
+        //remove it
+        newCategories.splice(newCategories.indexOf(e.target.value),1);
+       }else{
      //else add it in the category array,
-     
-  }
+        newCategories.push(e.target.value)
+      }
+      setCategory(newCategories);
+  };
+  console.log(category);
+  // if the category changes then update the value in url search params,
+  useEffect(()=>{
+    let params={};
+    params.category= category;
+    console.log(params);
+    setSearchParams(params);
+  },[category,setSearchParams]);
+
   return (
     <div>
       <h3>Filter Component</h3>
