c53e10c557b274427e7bb3abd4a771cbe71af900
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-10 11:58:59 +0530

    File Added

--- RCT-211/Day-7/book_management_system/src/Redux/store.js
+++ RCT-211/Day-7/book_management_system/src/Redux/store.js
@@ -1,11 +1,13 @@
-import {legacy_createStore,compose, applyMiddleware} from "redux";
-import { reducer } from "./AppData/reducer";
+import {legacy_createStore,compose, applyMiddleware, combineReducers} from "redux";
+import { reducer as AppReducer } from "./AppData/reducer";
+import { reducer as AuthReducer } from "./AuthData/reducer";
 import thunk from "redux-thunk";
 
+const rootReducer = combineReducers({AuthReducer,AppReducer});
 const composeEnhancer= window.
 __REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose;
 const store = legacy_createStore(
-    reducer,
+    rootReducer,
     composeEnhancer(applyMiddleware(thunk))
 );
 export {store};
