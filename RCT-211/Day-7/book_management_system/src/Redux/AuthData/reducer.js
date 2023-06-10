a24e1a9f1c9c54796c20ed1fc7823bb21c1ec682
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-10 11:40:49 +0530

    File Added

--- RCT-211/Day-7/book_management_system/src/Redux/AuthData/reducer.js
+++ RCT-211/Day-7/book_management_system/src/Redux/AuthData/reducer.js
@@ -11,5 +11,21 @@ const reducer = ( oldState=initialState,action )=>{
     switch(type){
         case types.USER_LOGIN_REQUEST:
             return {...oldState,isAuthLoading:true };
+        case types.USER_LOGIN_SUCCESS:
+            return {
+                ...oldState,
+                isAuthLoading: false,
+                token:payload,
+                isAuth:true,
+            };
+        case types.USER_LOGIN_ERROR:
+            return{
+                ...oldState,
+                isAuthLoading:false,
+                isAuthError:true,
+            };
+        default:
+            return oldState;
     }
-}
+};
+export { reducer };
