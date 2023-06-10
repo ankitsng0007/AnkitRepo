3f25cf4b0b5b2a1aa1075994d599a913f88710b8
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-10 12:23:02 +0530

    File Added

--- RCT-211/Day-7/book_management_system/src/Pages/LogIn.jsx
+++ RCT-211/Day-7/book_management_system/src/Pages/LogIn.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LogIn() {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
   return (
     <div>
-      
+      <form>
+        <div>
+          <label>Enter Email</label>
+          <input 
+          type='email' 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)}/>
+        </div>
+        <div>
+          <label>Enter Password</label>
+          <input 
+          type='password' 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)}/>
+        </div>
+        <button>Log In</button>
+      </form>
     </div>
   );
 }
