075d85f36969380a2d0d40571035554010fd460d
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-11 10:07:50 +0530

    File added

--- RCT-211/Day-7/book_management_system/src/Components/RequireAuth.jsx
+++ RCT-211/Day-7/book_management_system/src/Components/RequireAuth.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router';
 
-const RequireAuth = () => {
+const RequireAuth = ({children}) => {
     const isAuth = useSelector((store)=>store.AuthReducer.isAuth);
   if(!isAuth){
     return <Navigate to="/login" />
   }
-  return 
-  Children;
-}
+  return children;
+};
 
 export default RequireAuth;
--- RCT-211/Day-7/book_management_system/src/Pages/MainRoutes.jsx
+++ RCT-211/Day-7/book_management_system/src/Pages/MainRoutes.jsx
@@ -4,12 +4,19 @@ import Books from './Books';
 import SingleBook from './SingleBook';
 import EditBook from './EditBook';
 import LogIn from './LogIn';
+import RequireAuth from '../Components/RequireAuth';
 
 function MainRoutes() {
   return (
     <Routes>
         <Route path='/' element={<Books />} />
-        <Route path='/books/:id' element={<SingleBook />} />
+        <Route 
+        path='/books/:id' 
+        element={
+          <RequireAuth>
+        <SingleBook />
+        </RequireAuth>
+        } />
         <Route path='/books/:id/edit' element={<EditBook />} />
         <Route path='/login' element={<LogIn />} />
         <Route path='*' element={<h3>Page Not Found</h3>} />
