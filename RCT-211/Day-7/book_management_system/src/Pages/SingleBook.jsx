663993400a08065b6b2f4979d88b714cda5b1f54
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-11 09:43:26 +0530

    File added

--- RCT-211/Day-7/book_management_system/src/Pages/SingleBook.jsx
+++ RCT-211/Day-7/book_management_system/src/Pages/SingleBook.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router';
 
 function SingleBook() {
+  const isAuth = useSelector((store)=>store.AuthReducer.isAuth);
+  if(!isAuth){
+    return <Navigate to="/login" />
+  }
   return (
     <div>
       
