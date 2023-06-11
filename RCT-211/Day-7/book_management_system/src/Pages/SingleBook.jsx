99e719c1c4638bbb630748b4908a3ca9fa97d896
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-11 23:13:42 +0530

    File added

--- RCT-211/Day-7/book_management_system/src/Pages/SingleBook.jsx
+++ RCT-211/Day-7/book_management_system/src/Pages/SingleBook.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, useParams } from 'react-router';
 
@@ -6,6 +6,13 @@ function SingleBook() {
   const {id} = useParams();
   const books = useSelector((store)=>store.AppReducer.books);
   const [currentBook,setCurrentBook] = useState({});
+
+  useEffect(()=>{
+    if(id){
+      const book = books.find((item)=>item.id ===Number(id));
+      console.log(book);
+    }
+  },[])
   return (
     <div>
       
