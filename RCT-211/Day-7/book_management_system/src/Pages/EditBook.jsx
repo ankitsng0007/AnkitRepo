3433b0e08e08509b4f6b38b9066e4cf1886eb394
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-12 09:58:53 +0530

    File added

--- RCT-211/Day-7/book_management_system/src/Pages/EditBook.jsx
+++ RCT-211/Day-7/book_management_system/src/Pages/EditBook.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { getBooks } from '../Redux/AppData/action';
 
 function EditBook() {
+  const {id} = useParams();
+  const books = useSelector((store)=>store.AppReducer.books);
+  const [currentBook,setCurrentBook] = useState({});
+  const dispatch = useDispatch();
+
+  //fetching the data -> get all the books.
+  useEffect(()=>{
+    if(books.length === 0){
+      dispatch(getBooks());
+    }
+  },[books.length, dispatch]);
+
+  //get the data from the redux store.
+  //find the book from the list of book.
+  
+
   return (
     <div>
       
