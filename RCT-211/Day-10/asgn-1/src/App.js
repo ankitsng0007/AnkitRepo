b192a87c641f5fc87bc262ed7c20d58615f373c7
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-16 12:05:19 +0530

    file added

--- RCT-211/Day-10/asgn-1/src/App.js
+++ RCT-211/Day-10/asgn-1/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import useTimeout from './Hooks/useTimeout';
+import TimeoutComp from './Components/TimeoutComp';
 
 function App() {
   const showText = useTimeout();
 
   return (
     <div className="App">
-  {showText && <h1>App Component</h1>}    
+  <h1>App Component</h1>
+  <TimeoutComp/>  
     </div>
   );
 }
