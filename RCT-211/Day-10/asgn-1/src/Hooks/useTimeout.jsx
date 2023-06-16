a47ed949966ae29665264bf02815161030c8b69f
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-16 11:56:03 +0530

    file added

---
+++ RCT-211/Day-10/asgn-1/src/Hooks/useTimeout.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+const useTimeout = () => {
+    const [showText,setShowText]  = useState(false);
+
+    useEffect(()=>{
+    const id = setTimeout(()=>{
+        setShowText((prev)=>!prev);
+    },3000);
+    return ()=>{
+        clearTimeout(id);
+    };
+},[])
+  return showText;
+};
+
+export default useTimeout;
