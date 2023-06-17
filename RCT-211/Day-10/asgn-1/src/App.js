c8aed20cd44238b584c90b38f089183cefb8c8c4
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-17 10:25:58 +0530

    file added

--- RCT-211/Day-10/asgn-1/src/App.js
+++ RCT-211/Day-10/asgn-1/src/App.js
@@ -2,9 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import useTimeout from './Hooks/useTimeout';
 import TimeoutComp from './Components/TimeoutComp';
+import { useState } from 'react';
 
 function App() {
  const showText = useTimeout();
+ const [scrollNum,setScrollNum]=useState(0);
+
+ const handleScrollEvent =()=>{
+  setScrollNum((prev)=> prev + 1);
+ };
 
   return (
     <div className="App">
--- RCT-211/Day-10/asgn-1/src/Hooks/useDebounce.jsx
+++ RCT-211/Day-10/asgn-1/src/Hooks/useDebounce.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react";
 
-const useDebounce = (func,delay){
+const useDebounce = (func,delay)=>{
     const debounceId = useRef();
     useEffect (()=>{
         clearTimeout(debounceId.current);
