f047dd7d2fef0195b3262ec46d62edff287aed9c
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-19 10:24:26 +0530

    File added

--- RCT-211/Day-11/performance_optimization/src/Components/Todos.jsx
+++ RCT-211/Day-11/performance_optimization/src/Components/Todos.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoItems from './TodoItems';
 
+
+const initialState = [
+  {id:1, title:"Learn React", status:false },
+  {id:1, title:"Learn Java", status:false },
+  {id:1, title:"Learn Python", status:false },
+]
 const Todos = () => {
+  const [todo,setTodo] = useState(initialState);
+
   return (
     <div>
       <h1>Todos</h1>
+      <input />
+      <button>Add</button>
+      {Todos.length && Todos.map(item =>{
+        return (<TodoItems key={item.id} {...item}/>)
+      })}
+
     </div>
   );
 }
