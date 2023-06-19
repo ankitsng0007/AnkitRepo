0c2d656e987f354f91e1764c7a200cf4e33ea508
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-19 10:06:15 +0530

    File added

--- RCT-211/Day-11/performance_optimization/src/Components/Todos.jsx
+++ RCT-211/Day-11/performance_optimization/src/Components/Todos.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Todo = () => {
+const Todos = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Todo = () => {
   );
 }
 
-export default Todo;
+export default Todos;
