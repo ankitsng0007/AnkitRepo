59aa0d9c96c61271733cf4746f0c81c5838b791a
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-20 09:36:27 +0530

    File added

--- RCT-211/Day-11/performance_optimization/src/Components/TodoItems.jsx
+++ RCT-211/Day-11/performance_optimization/src/Components/TodoItems.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+const expensiveOperation =(ms) =>{
+    const start = Date.now();
+    while(Date.now()-start < ms){
+        continue;
+    }
+    return true;
+};
+
 const TodoItems = ({ id,title,status }) => {
-  return (
+  //syncronus function that takes some time
+  expensiveOperation(200);
+
+    return (
     <div style={{display:"flex", justifyContent:"center" }}>
       <div>{title}</div>
       <div>{status ? "True" : "False" }</div>
