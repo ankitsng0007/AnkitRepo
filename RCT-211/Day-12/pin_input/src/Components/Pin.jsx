9d6eab5f523e4d4e3bf0b49fbb72cd02c6442d65
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-23 11:39:57 +0530

    File added

--- RCT-211/Day-12/pin_input/src/Components/Pin.jsx
+++ RCT-211/Day-12/pin_input/src/Components/Pin.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
-
-const Pin = ({length}) => {
+import React, { useRef, useState } from 'react';
+import PropTypes from "prop-types";
+const Pin = ({length,maxLength}) => {
     const [inputBoxLength] = useState(new Array(length).fill(""));
-
+    const inputRef = useRef([]);
+    console.log(inputRef);
   return (
     <div>
-      {inputBoxLength.map((_,index)=>{
-        return <input key={index}/>
+      {inputBoxLength.map((item,index)=>{
+        return <input ref={(inputRefElement)=>{
+            console.log(inputRefElement,index);
+            inputRef.current[index] = inputRefElement;
+            console.log(inputRef.current);
+        }}
+            key={index} maxLength={maxLength}/>
       })}
     </div>
   );
 }
 
 export default Pin;
+
+Pin.propTypes = {
+    length: PropTypes.number.isRequired,
+};
