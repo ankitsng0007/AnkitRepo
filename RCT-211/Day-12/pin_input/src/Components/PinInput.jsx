d92fbecfa5d8d1ee9b17c6c5b487e5c399657d7d
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-24 09:16:57 +0530

    File Added

--- RCT-211/Day-12/pin_input/src/Components/PinInput.jsx
+++ RCT-211/Day-12/pin_input/src/Components/PinInput.jsx
@@ -1,11 +1,19 @@
 import React ,{forwardRef} from 'react';
 
 const PinInput = forwardRef(({perInputBox,onChange},ref) => {
+
+  const handleKeyUp = (e)=>{
+    console.log(e);
+    //if the user has clicked on the backspace button;
+    //then invoke backspaceHandler;
+    //else invoke default onchangehandler
+  }
   return(
     <input 
     ref={ref} 
     maxLength={perInputBox}
     onChange={onChange}
+    onKeyUp={handleKeyUp}
     />
   )
 })
