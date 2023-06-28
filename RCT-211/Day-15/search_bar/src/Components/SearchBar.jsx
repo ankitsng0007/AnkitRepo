22487f4c2ce4e2c7075273bafaf6a970fb449802
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-28 23:59:18 +0530

    File Added

--- RCT-211/Day-15/search_bar/src/Components/SearchBar.jsx
+++ RCT-211/Day-15/search_bar/src/Components/SearchBar.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import styled from "styled-components";
 
-const SearchBar = () => {
+const SearchBar = ({setQurey}) => {
   return (
     <div>
-      
+        <SearchBarWrapper>
+      <input />
+        </SearchBarWrapper>
     </div>
   );
 }
 
+const SearchBarWrapper = styled.div`
+border:1p solid red;
+`;
 export default SearchBar;
