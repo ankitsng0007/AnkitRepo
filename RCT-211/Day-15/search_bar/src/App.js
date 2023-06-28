e7eaec2a239c534b150590f767ad164c06c84c17
ankitsng0007 <ankitsng0007@gmail.com>
2023-06-28 23:39:52 +0530

    File Added

--- RCT-211/Day-15/search_bar/src/App.js
+++ RCT-211/Day-15/search_bar/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './Components/SearchBar';
+import { useState } from 'react';
 
 function App() {
+  const [qurey,setQurey] = useState("");
   return (
     <div className="App">
       <h1>Search Bar</h1>
+      <h3>Search Qurey : {qurey}</h3>
+      <SearchBar />
     </div>
   );
 }
