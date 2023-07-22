5e4cdd85f824e15484a7e0a0972ddfc0e2d04607
ankitsng0007 <ankitsng0007@gmail.com>
2023-07-22 06:28:58 +0530

    file added

--- RCT-211/Dey-17/Asgn-1/src/1.ts
+++ RCT-211/Dey-17/Asgn-1/src/1.ts
@@ -1,1 +1,13 @@
 console.log("HELLO");
+
+
+let students:{
+    rollNo:number;
+    name:string;
+    batch:"RCT-101" | "RCT-201";
+}[] = [
+    { rollNo:1, name:"Ankit", batch:"RCT-101", class:1 },
+    { rollNo:2, name:"Shivam", batch:"RCT-101", class:1 },
+    { rollNo:3, name:"Rajeev", batch:"RCT-201", class:2 },
+    { rollNo:4, name:"Satyam", batch:"RCT-301", class:2 },
+]
