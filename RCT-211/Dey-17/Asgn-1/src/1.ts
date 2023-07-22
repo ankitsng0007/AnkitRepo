c558b58210ff02ebfc3b9eb03d104a5b11eb291a
ankitsng0007 <ankitsng0007@gmail.com>
2023-07-22 07:37:40 +0530

    file added

--- RCT-211/Dey-17/Asgn-1/src/1.ts
+++ RCT-211/Dey-17/Asgn-1/src/1.ts
@@ -10,7 +10,25 @@ let students:{
 }[] = [
     { rollNo:1, name:"Ankit", batch:"RCT-101", class:1 },
     { rollNo:2, name:"Shivam", batch:"RCT-101", class:1 },
-    { rollNo:3, name:"Rajeev", batch:"RCT-201", class:2 },
+    { rollNo:3, fullName:"Rajeev", batch:"RCT-201", class:2 },
     { rollNo:4, name:"Satyam", batch:"RCT-301", class:2 },
     { rollNo:1, name:"Ankit", batch:"RCT-101", class:1, age:25 }
+];
+
+let cartItems =[
+    {
+    id:1, 
+    products:[{id:1,name:"Towel", price:250,quantity:2}], 
+    total:500
+},
+{
+    id:1, 
+    products:[{id:1,name:"Towel", price:250,quantity:3}], 
+    total:750
+},{
+    id:1, 
+    products:[{id:1,name:"Towel", price:250,quantity:2},
+    {id:1,name:"Shampoo", price:10,quantity:2}], 
+    total:520
+}
 ]
