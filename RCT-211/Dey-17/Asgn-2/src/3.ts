81ff8adbc3cc458bb586353da735ee770ded00bb
ankitsng0007 <ankitsng0007@gmail.com>
2023-07-27 10:29:17 +0530

    file added

---
+++ RCT-211/Dey-17/Asgn-2/src/3.ts
@@ -0,0 +1,21 @@
+// Interface
+
+
+
+type person = {
+    id: number;
+    name: string;
+};
+
+interface PersonX {
+    id: number;
+    name: string;
+};
+
+interface PersonX {
+    age: number;
+};
+
+interface PersonX {
+    address: string;
+};
