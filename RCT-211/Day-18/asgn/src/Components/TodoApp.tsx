82be817b54e85734d2ee7f253cbf6062d266655a
ankitsng0007 <ankitsng0007@gmail.com>
2023-08-03 16:04:48 +0530

    File Added

--- RCT-211/Day-18/asgn/src/Components/TodoApp.tsx
+++ RCT-211/Day-18/asgn/src/Components/TodoApp.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getTodos } from './api';
+import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
 
 const TodoApp = () => {
   const [todos, setTodos] = useState( [] );
@@ -11,7 +13,13 @@ const TodoApp = () => {
   }, []);
   return (
     <div>
-      TodoApp
+      <h1>Todo App</h1>
+      <TodoInput />
+      {todos.map((item)=> (
+        <div key={item.id}>
+          <TodoItem />
+        </div>
+      ))}
     </div>
   );
 }
