6754cc0ae5ec996c823905d5571cbdd519b189e3
ankitsng0007 <ankitsng0007@gmail.com>
2023-08-03 16:06:47 +0530

    File Added

--- RCT-211/Day-18/asgn/src/App.tsx
+++ RCT-211/Day-18/asgn/src/App.tsx
@@ -1,16 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoApp from './Components/TodoApp';
-import { getTodos } from './Components/api';
 
 function App() {
-
-  useEffect(()=>{
-    getTodos().then((d)=>{
-      console.log(d);
-    });
-  });
   return (
     <div className="App">
       <TodoApp />
