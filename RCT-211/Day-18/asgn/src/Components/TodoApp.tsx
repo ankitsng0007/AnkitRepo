8a49e3241cbe5f04c6831ac9c41f8c8912ec27d4
ankitsng0007 <ankitsng0007@gmail.com>
2023-08-03 16:00:13 +0530

    File Added

--- RCT-211/Day-18/asgn/src/Components/TodoApp.tsx
+++ RCT-211/Day-18/asgn/src/Components/TodoApp.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getTodos } from './api';
 
 const TodoApp = () => {
+  const [todos, setTodos] = useState( [] );
+
+  useEffect(()=>{
+    getTodos().then((d) => {
+      console.log(d);
+    });
+  }, []);
   return (
     <div>
       TodoApp
