95c1b77c7f61eeff8368dd8ee779165ef6f6d284
ankitsng0007 <ankitsng0007@gmail.com>
2023-08-04 05:58:10 +0530

    file added

--- RCT-211/Day-18/asgn/src/Components/api.ts
+++ RCT-211/Day-18/asgn/src/Components/api.ts
@@ -1,7 +1,8 @@
-import axios from "axios"
+import axios ,{ AxiosResponse } from "axios"
+import { Todo } from "./constants";
 
 
 export const getTodos = async() => {
-   let res = await axios.get("http://localhost:8080/todos");
+   let res : AxiosResponse< Todo[] > = await axios.get("http://localhost:8080/todos");
    return res.data ;
 };
