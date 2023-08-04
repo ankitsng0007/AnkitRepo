f7abbbbaf734b468963b2b1a36755f5f1dc0d9fe
ankitsng0007 <ankitsng0007@gmail.com>
2023-08-04 07:31:54 +0530

    file added

--- RCT-211/Day-18/asgn/src/Components/api.ts
+++ RCT-211/Day-18/asgn/src/Components/api.ts
@@ -11,6 +11,7 @@ export const addTodo = async ( message:string, type:TodoType): Promise<Todo> =>{
    let res : AxiosResponse<Todo> = await axios.post("http://localhost:8080/todos",{
       message,type,likes: 0,
    });
+   return res.data ;
 }
 export const updateTodo = async ( changedTodo : Todo) : Promise<Todo> =>{
    let res : AxiosResponse< Todo > = await axios.patch(`http://localhost:8080/todos${changedTodo.id}`,
