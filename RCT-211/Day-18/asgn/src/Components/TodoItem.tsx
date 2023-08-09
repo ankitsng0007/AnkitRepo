70971cc6850cb2fa9d89b28f1d2582dac8e556c5
ankitsng0007 <ankitsng0007@gmail.com>
2023-08-09 13:53:52 +0530

    File Added

--- RCT-211/Day-18/asgn/src/Components/TodoItem.tsx
+++ RCT-211/Day-18/asgn/src/Components/TodoItem.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Todo } from './constants';
+import { Todo, colorMap } from './constants';
 
 type TodoItemProps = Todo ;
 const TodoItem = (props : TodoItemProps) => {
   return (
-    <div style={}>
+    <div style={{padding:"5px",backgroundColor: colorMap[props.type]}}>
       {props.message} - {props.likes}
     </div>
   );
