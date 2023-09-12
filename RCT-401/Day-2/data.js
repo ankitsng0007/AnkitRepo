0a9753e137b384f54a0bbdb402c16359befb2279
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-13 00:25:01 +0530

    File added

--- RCT-401/Day-2/data.js
+++ RCT-401/Day-2/data.js
@@ -3,4 +3,15 @@ const sum = (a,b) => {
     console.log(a+b);
 }
 
+const diff = (a,b) => {
+    console.log(a-b);
+}
+
+const pro = (a,b) => {
+    console.log(a*b);
+}
+
+const div = (a,b) => {
+    console.log(a/b);
+}
 module.exports = sum ;
