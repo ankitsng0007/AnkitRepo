35eaaa181f2fbe09c26916cca3b5c32a9f25b6ce
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-14 18:02:15 +0530

    file added

--- RCT-401/Day-3/Asgn-2/index.js
+++ RCT-401/Day-3/Asgn-2/index.js
@@ -9,12 +9,35 @@ const fs = require("fs")
  //   }
 //})
 
-let data;
-try{
-    data = fs.readFileSync("./lecture.txt",{encoding:"utf-8"})
-    console.log(data);
-} catch(err){
-    console.log(err)
-}
+//let data;
+//try{
+//    data = fs.readFileSync("./lecture.txt",{encoding:"utf-8"})
+//    console.log(data);
+//} catch(err){
+//    console.log(err)
+//}
 
+//fs.writeFile("./lecture.txt","This is my first project",(err)=>{
+//    if(err){
+//        console.log("cannot write in the file")
+//        console.log(err)
+//    } else {
+ //       console.log("Data has been written")
+ //   }
+//})
+
+//try{
+//    fs.writeFileSync("./leture.txt","This is my second project")
+//    console.log("Data has been entered")
+//} catch(err){
+//    console.log(err)
+//}
+
+fs.appendFile("./lecture.txt","\nThis is my third Project\n",(err)=>{
+    if(err){
+        console.log(err)
+    } else {
+        console.log("Appended")
+    }
+})
 console.log("Bye Guys!!")
