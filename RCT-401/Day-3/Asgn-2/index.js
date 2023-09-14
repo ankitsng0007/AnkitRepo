dd53d7f9c1ff75d9547c1f402ccd20898b18f8ad
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-14 17:35:07 +0530

    file added

--- RCT-401/Day-3/Asgn-2/index.js
+++ RCT-401/Day-3/Asgn-2/index.js
@@ -1,12 +1,20 @@
 const fs = require("fs")
 
-fs.readFile("./lecture.txt",{encoding:"utf-8"},(err,data)=>{
-    if(err){
-        console.log("file cannot be read")
-        console.log(err)
-    } else {
-        console.log(data)
-    }
-})
+//fs.readFile("./lecture.txt",{encoding:"utf-8"},(err,data)=>{
+//    if(err){
+//        console.log("file cannot be read")
+//        console.log(err)
+//    } else {
+ //       console.log(data)
+ //   }
+//})
+
+let data;
+try{
+    data = fs.readFileSync("./lecture.txt",{encoding:"utf-8"})
+    console.log(data);
+} catch(err){
+    console.log(err)
+}
 
 console.log("Bye Guys!!")
