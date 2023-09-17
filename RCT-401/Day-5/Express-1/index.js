f27fe1b9b72edd2c282d6da4e22c76011ea2a308
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-17 19:40:09 +0530

    File added

--- RCT-401/Day-5/Express-1/index.js
+++ RCT-401/Day-5/Express-1/index.js
@@ -1,9 +1,19 @@
 const express = require("express")
 
 const app = express();
+app.use(express.json())
 
+app.get("/",(req,res)=>{
+    res.end("Hello World")
+})
+app.get("/data",(req,res)=>{
+    res.end("Data will be Entered")
+})
 
-
+app.post("/addData",(req,res)=>{
+    console.log(req.body)
+    res.end("Data has been recorded")
+})
 
 app.listen(3500,()=>{
     console.log("Running at the port 3500")
