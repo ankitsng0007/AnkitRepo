82dfd5ea607f606c7a9f12440568d92ab92d13d6
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-18 00:03:00 +0530

    File added

--- RCT-401/Day-5/Express-1/index.js
+++ RCT-401/Day-5/Express-1/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const fs = require("fs")
 
 const app = express();
 app.use(express.json())
@@ -6,13 +7,10 @@ app.use(express.json())
 app.get("/",(req,res)=>{
     res.end("Hello World")
 })
-app.get("/data",(req,res)=>{
-    res.end("Data will be Entered")
-})
-
-app.post("/addData",(req,res)=>{
-    console.log(req.body)
-    res.end("Data has been recorded")
+app.get("/allData",(req,res)=>{
+    const data = fs.readFileSync("./db.json","utf-8")
+    console.log(data)
+    res.send("You got data in terminal.")
 })
 
 app.listen(3500,()=>{
