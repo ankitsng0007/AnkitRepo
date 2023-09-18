daeabbeb62370e9c2b20104dc9393c334675de7f
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-18 12:43:13 +0530

    File Added

--- RCT-401/Day-5/Express-1/index.js
+++ RCT-401/Day-5/Express-1/index.js
@@ -20,6 +20,31 @@ app.get("/allStudent",(req,res)=>{
     res.send(parsed_data.student)
 })
 
+app.post("/addStudent",(req,res)=>{
+    //reading the data first.
+    const data = fs.readFileSync("./db.json","utf-8")
+    //Parsing the data to add a new student
+    const parsed_data = JSON.parse(data)
+    //adding a new student
+    parsed_data.student.push(req.body)
+    fs.writeFileSync("./db.json",JSON.stringify(parsed_data))
+    res.send("required Data Updated")
+})
+app.post("/addTeacher",(req,res)=>{
+    //reading the data first.
+    const data = fs.readFileSync("./db.json","utf-8")
+    //Parsing the data to add a new teacher
+    const parsed_data = JSON.parse(data)
+    //adding a new teacher
+    parsed_data.teacher.push(req.body)
+    fs.writeFileSync("./db.json",JSON.stringify(parsed_data))
+    res.send("required Data Updated")
+})
+
+app.delete("/deleteStudent",(req,res)=>{
+
+})
+
 app.listen(3500,()=>{
     console.log("Running at the port 3500")
 })
