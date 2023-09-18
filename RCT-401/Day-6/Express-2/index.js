abaa2d45870ba6670f0b8792588ab6e4a8f00250
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-19 01:35:33 +0530

    file added

--- RCT-401/Day-6/Express-2/index.js
+++ RCT-401/Day-6/Express-2/index.js
@@ -1,15 +1,10 @@
 const express = require("express")
 const fs = require("fs")
+const {timeLogger} = require("./Middlewares/timeLogger")
 
 const app = express()
 //app.use(middleware)
 
-const timeLogger = (req,res,next)=>{
-    const startTime = new Date().getTime()
-    next()
-    const endTime = new Date().getTime()
-    console.log(endTime-startTime)
-}
 
 app.get("/",(req,res)=>{
     res.send("Wellcome Page")
