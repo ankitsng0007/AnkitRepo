aaea323495b50ad41c028c73adf6ea0e3a31de67
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-18 12:58:19 +0530

    File Added

--- RCT-401/Day-6/Express-2/index.js
+++ RCT-401/Day-6/Express-2/index.js
@@ -2,7 +2,18 @@ const express = require("express")
 
 const app = express()
 
-
+app.get("/",(req,res)=>{
+    res.send("Wellcome Page")
+})
+app.get("/about",(req,res)=>{
+    res.send("About Page")
+})
+app.get("/contact",(req,res)=>{
+    res.send("Contact Page")
+})
+app.get("/data",(req,res)=>{
+    res.send("Data will be sent")
+})
 
 app.listen(4500,()=>{
     console.log("Running at port 4500")
