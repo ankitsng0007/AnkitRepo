2a0c4e38caed94f849851fbb3bbaf1f8e596f6b8
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-19 14:51:46 +0530

    file added

--- RCT-401/Day-6/Express-2/Routes/student.router.js
+++ RCT-401/Day-6/Express-2/Routes/student.router.js
@@ -5,3 +5,13 @@ const studentRouter = express.Router()
 studentRouter.get("/students",(req,res)=>{
     res.send("All Students")
 })
+
+studentRouter.post("/addStudent",(req,res)=>{
+    console.log(req.body)
+    res.send("Student Added")
+})
+
+module.exports={
+    studentRouter
+    
+}
