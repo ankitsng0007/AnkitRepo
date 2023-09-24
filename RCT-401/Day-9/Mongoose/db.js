7a5a1ee717c59e3f2bf874cb23611eb0ea6088b9
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-25 01:09:35 +0530

    file added

--- RCT-401/Day-9/Mongoose/db.js
+++ RCT-401/Day-9/Mongoose/db.js
@@ -4,8 +4,10 @@ const mongoose = require("mongoose")
 
 const main = async ()=>{
     try{
-    const connection = mongoose.connect("mongodb://127.0.0.1:27017/")
+    const connection = await mongoose.connect("mongodb://127.0.0.1:27017/")
     console.log("connected to DBs")
+    connection.disconnect()
+    console.log("Disconnected")
 } catch(err){
     console.log("Cannot connect")
     console.log(err)
