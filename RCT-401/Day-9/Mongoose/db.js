bd043b3f2d2e76e5bc12febec8f4df6054d2de94
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-25 00:54:42 +0530

    file added

--- RCT-401/Day-9/Mongoose/db.js
+++ RCT-401/Day-9/Mongoose/db.js
@@ -3,5 +3,7 @@ const mongoose = require("mongoose")
 
 
 const main = ()=>{
-    const connection = mongoose.connect()
+    const connection = mongoose.connect("mongodb://127.0.0.1:27017/")
+    console.log("connected to DBs")
 }
+main();
