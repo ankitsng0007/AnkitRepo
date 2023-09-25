257f8594c6da489ced19b72309e1483a93cb6d45
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-25 23:59:30 +0530

    file added

--- RCT-401/Day-9/Mongoose/db.js
+++ RCT-401/Day-9/Mongoose/db.js
@@ -6,7 +6,13 @@ const main = async ()=>{
     try{
     const connection = await mongoose.connect("mongodb://127.0.0.1:27017/school")
     console.log("connected to DBs")
-    await studentModel.insertMany([{name:"Ankit",age:25,city:"Bangalore",is_married:false}])
+    //await studentModel.insertMany([{name:"Ankit",age:25,city:"Bangalore",is_married:false}])
+    const student = new StudentModel({
+        name:"Pulkit",
+        age:26,
+        city:"Patna",
+        is_married:true
+    })
     console.log("Added to DB")
     connection.disconnect()
     console.log("Disconnected")
