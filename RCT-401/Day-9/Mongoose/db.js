2cb52cfbef7233ee352d20c92de8d7bf9d5121b0
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-26 11:36:31 +0530

    file addded

--- RCT-401/Day-9/Mongoose/db.js
+++ RCT-401/Day-9/Mongoose/db.js
@@ -7,17 +7,17 @@ const main = async ()=>{
     const connection = await mongoose.connect("mongodb://127.0.0.1:27017/school")
     console.log("connected to DBs")
     //await studentModel.insertMany([{name:"Ankit",age:25,city:"Bangalore",is_married:false}])
-    //const student = new studentModel({
-    //    name:"Pulkit",
-    //    age:26,
-    //    city:"Patna",
-    //    is_married:true
-    //})
-   // await student.save()
-   // console.log("Added to DB")
-   console.log("Following are the student from database")
-   const students = await studentModel.find()
-   console.log(students)
+    const student = new studentModel({
+        name:"Priya",
+        age:22,
+        city:"Delhi",
+        is_married:true
+    })
+    await student.save()
+    console.log("Added to DB")
+   //console.log("Following are the student from database")
+   //const students = await studentModel.find()
+  // console.log(students)
     connection.disconnect()
     console.log("Disconnected")
 } catch(err){
@@ -28,10 +28,10 @@ const main = async ()=>{
 main();
 
 const studentSchema = mongoose.Schema({
-    name:String,
-    age:Number,
-    city:String,
-    is_married:Boolean
+    name:{type:String,required:true},
+    age:{type:Number,required:true},
+    city:{type:String,required:true},
+    is_married:{type:Boolean,required:true}
 },{
     versionKey:false
 })
