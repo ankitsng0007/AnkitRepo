c24140ba564565200280db7d233e853565b23b04
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-26 13:44:32 +0530

    file addded

--- RCT-401/Day-10/Mongoose-2/index.js
+++ RCT-401/Day-10/Mongoose-2/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
+const {connection} = require("./db")
 
 const app = express()
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.send("Wellcome")
@@ -10,6 +12,18 @@ app.get("/heros",(req,res)=>{
     res.send("data of all the super heros")
 })
 
-app.listen(4500,()=>{
+app.post("/addhero",(req,res)=>{
+    const data = req.body
+    console.log(data)
+    res.send("Added the Hero")
+})
+app.listen(4500,async ()=>{
+    try{
+        await connection
+        console.log("Connected to db")
+    }catch(err){
+        console.log("Error while connecting to db")
+        console.log(err)
+    }
     console.log("server is running on the port 4500")
 })
