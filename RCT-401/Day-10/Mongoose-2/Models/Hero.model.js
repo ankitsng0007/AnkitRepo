ae447783d04536e8429dc340f3ba63950cc701bd
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-27 16:05:13 +0530

    file addded

---
+++ RCT-401/Day-10/Mongoose-2/Models/Hero.model.js
@@ -0,0 +1,15 @@
+
+const heroSchema = mongoose.Schema({
+    name:String,
+    city:String,
+    power:Number,
+    villian:String,
+    language:String,
+    is_active:Boolean
+})
+
+const heroModel = mongoose.model("hero",heroSchema)
+
+module.exports= {
+    heroModel
+}
