015241d4c8e4255f8ddee3e2943705230d3cf1c0
ankitsng0007 <ankitsng0007@gmail.com>
2023-09-27 16:28:20 +0530

    file addded

--- RCT-401/Day-10/Mongoose-2/index.js
+++ RCT-401/Day-10/Mongoose-2/index.js
@@ -13,48 +13,6 @@ app.get("/heros",(req,res)=>{
     res.send("data of all the super heros")
 })
 
-app.get("/heroes",async (req,res)=>{
-    let query=req.query
-    try{
-     const heroes = await heroModel.find({query})
-     res.send(heroes)
-    }catch(err){
-        console.log(err)
-        res.send({"err":"Something went wrong"})
-    }
-})
-
-app.patch("/edithero/:id", async (req,res)=>{
-    const ID = req.params.id
-    const payload = req.body
-    try{
-        await heroModel.findByIdAndUpdate({_id:ID},payload)
-        res.send(`Update the hero data whose id is ${ID}`)
-    }catch(err){
-        console.log(err)
-        console.log({"err":"Somthing went wrong"})
-    }
-})
-
-app.delete("/edithero/:id", async (req,res)=>{
-    const ID = req.params.id
-    try{
-        await heroModel.findByIdAndDelete({_id:ID})
-        res.send(`Delete the hero data whose id is ${ID}`)
-    }catch(err){
-        console.log(err)
-        console.log({"err":"Somthing went wrong"})
-    }
-})
-
-app.post("/addhero",async (req,res)=>{
-    const data = req.body
-    const hero = new heroModel(data)
-    await hero.save()
-    console.log(hero)
-    res.send("Added the Hero")
-})
-
 
 app.listen(process.env.port,async ()=>{
     try{
