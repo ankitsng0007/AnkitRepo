3ff304f721ff897a36490ac30fa68b0e14156f45
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-05 12:14:57 +0530

    File added

--- RCT-401/Day-13/Authentiction/index.js
+++ RCT-401/Day-13/Authentiction/index.js
@@ -2,11 +2,17 @@ const express = require("express")
 
 const app = express()
 
+app.get("/",(req,res)=>{
+    res.send("Wellcome")
+})
+
 app.post("/register",(req,res)=>{
     res.send("Registered")
 })
 
-
+app.post("/login",(req,res)=>{
+    res.send("Logged In")
+})
 
 app.listen(8080,()=>{
     console.log("Running at 8080")
