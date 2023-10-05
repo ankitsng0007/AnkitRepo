86600da5907cd455c39adef33dbd6dba35bd1492
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-05 16:02:47 +0530

    File added

--- RCT-401/Day-13/Authentiction/index.js
+++ RCT-401/Day-13/Authentiction/index.js
@@ -1,18 +1,36 @@
 const express = require("express")
 const {connection} = require("./Config/db")
+const { userModel } = require("./Models/user.model")
 
 const app = express()
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.send("Wellcome")
 })
 
-app.post("/register",(req,res)=>{
-    res.send("Registered")
+app.post("/register", async(req,res)=>{
+    const payload = req.body
+    try{
+        const user = new userModel(payload)
+        await user.save()
+        res.send("Registered")
+    }catch(err){
+        res.send("Eror in Registering the user")
+        console.log(err)
+    }
 })
 
-app.post("/login",(req,res)=>{
-    res.send("Logged In")
+app.post("/login", async(req,res)=>{
+    const {email,pass} = req.body
+    try{
+        const user = await userModel.find({email,pass})
+        console.log(user)
+        res.send("Working on it")
+    }catch(err){
+        res.send("Something Went Wrong")
+        console.log(err)
+    }
 })
 
 app.listen(8080, async()=>{
