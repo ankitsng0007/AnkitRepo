14361473513c1dda1f32b4534268d70a209fd810
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-05 19:13:28 +0530

    File added

--- RCT-401/Day-13/Authentiction/index.js
+++ RCT-401/Day-13/Authentiction/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const {connection} = require("./Config/db")
-
+const {UserModel} = require("./Models/User.model")
 
 const app = express()
 app.use(express.json())
