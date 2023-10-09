94cecf7f8c9571fabd9e2cfdc578f3102e3c4cc9
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-10 02:10:53 +0530

    File added

--- RCT-401/Day-13/Athentication/frontend/src/components/Register.jsx
+++ RCT-401/Day-13/Athentication/frontend/src/components/Register.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
 
 const Register = ()=>{
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [pass,setPass] = useState("");
+    const [age,setAge] = useState("");
+
     return(
     <div>
         <h2>Registration Page</h2>
+        <input type="text" placeholder="Enter Name" value={name} onChange={(e)=>setName(e.target.value)}/>
+        <input type="email" placeholder="Enter Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+        <input type="password" placeholder="Enter Password" value={pass} onChange={(e)=>setPass(e.target.value)}/>
+        <input type="number" placeholder="Enter Age" value={age} onChange={(e)=>setAge(e.target.value)}/>
+        <button>Submit</button>
     </div>
     )
 }
