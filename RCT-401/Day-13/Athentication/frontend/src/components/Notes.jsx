2e6587fed78c72ee59a5d578665da176e090fd91
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-12 23:51:37 +0530

    File DDED

--- RCT-401/Day-13/Athentication/frontend/src/components/Notes.jsx
+++ RCT-401/Day-13/Athentication/frontend/src/components/Notes.jsx
@@ -1,18 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Notes = () => {
+    const [notes,setNotes] = useState("")
+
     useEffect(()=>{
         fetch("http://localhost:8080/notes",{
             headers:{
                 "Authorization":localStorage.getItem("token")
             }
-        }).then(res=>res.json()).then(res=>console.log(res)).catch(err=>console.log(err))
+        })
+        .then(res=>res.json())
+        .then(res=>{
+            console.log(res)
+            setNotes(res)
+        })
+        .catch(err=>console.log(err))
     },[])
 
+    const DeleteNote=(noteId)=>{
+        fetch(`http://localhost:8080/notes/delete/${noteId}`,{
+            method:"DELETE",
+            headers:{
+                "Authorization":localStorage.getItem("token")
+            }
+        })
+    }
+
   return (
-    <div>
-      <h1>All Notes are in Console</h1>
-    </div>
+    <>
+      <h1>All Notes</h1>
+      {notes?notes.map((ele)=>{
+        return(
+            <>
+            <h2>Title:{ele.title}</h2>
+            <p>Note:{ele.note}</p>
+            <button onClick={()=>DeleteNote(ele._id)}>Delete</button>
+            </>
+        )
+      }):<h1>No Notes are available</h1>}
+    </>
   );
 }
 
